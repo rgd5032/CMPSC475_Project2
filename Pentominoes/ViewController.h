d95c2bd3eddd94acd9a5ae144e385703351e969d
ROBERT GERALD DICK <rgd5032@patteeww102-lab-mac51.clc-labs.its.psu.edu>
2013-09-08 23:55:23 -0400

    Initial Commit

---
+++ Pentominoes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pentominoes
+//
+//  Created by ROBERT GERALD DICK on 9/8/13.
+//  Copyright (c) 2013 ROBERT GERALD DICK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Pentominoes/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Pentominoes
+//
+//  Created by ROBERT GERALD DICK on 9/8/13.
+//  Copyright (c) 2013 ROBERT GERALD DICK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
