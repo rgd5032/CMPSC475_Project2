0290cdd59ca4e9cc5bf6c9d777c992a7df351e6a
ROBERT GERALD DICK <rgd5032@waring107-lab-mac149.clc-labs.its.psu.edu>
2013-09-12 18:29:35 -0400

    Solutions implemented. Animations implemented.

--- Pentominoes/ViewController.m
+++ Pentominoes/ViewController.m
@@ -12,13 +12,18 @@
 #define kPaddingBetweenPlayingPieces 30
 #define kPlayingPieceRowHeight 140
 #define kPaddingBetweenPiecesAndBoard 40
+#define kBoardBlockDimension 30
 
 @interface ViewController ()
 - (IBAction)boardButtonClicked:(id)sender;
+- (IBAction)solveButtonClicked:(id)sender;
+- (IBAction)resetButtonClicked:(id)sender;
 @property (weak, nonatomic) IBOutlet UIImageView *boardView;
 @property (strong, nonatomic) NSArray *boardImages;
-@property (strong, nonatomic) UIImage *currentBoardImage;
-@property (strong, nonatomic) NSMutableArray *playingPieces;
+@property (strong, nonatomic) NSMutableDictionary *playingPieces;
+@property (strong, nonatomic) NSArray *solutions;
+@property NSInteger currentBoardNumber;
+@property BOOL puzzleSolved;
 
 @end
 
@@ -37,12 +42,17 @@
                     [UIImage imageNamed:@"Board5.png"],
                     nil];
     [self createPlayingPieces];
+    
+    NSBundle *mainBundle = [NSBundle mainBundle];
+    NSString *path = [mainBundle pathForResource:@"Solutions" ofType:@"plist"];
+    self.solutions = [NSArray arrayWithContentsOfFile:path];
+    self.puzzleSolved = NO;
 }
 
 -(void)viewDidAppear:(BOOL)animated
 {
-    self.currentBoardImage = self.boardImages[0];
-    [self.boardView setImage: self.currentBoardImage];
+    self.currentBoardNumber = 0;
+    [self.boardView setImage: self.boardImages[self.currentBoardNumber]];
     [self placePlayingPieces];
 }
 
@@ -54,29 +64,15 @@
 
 -(void)createPlayingPieces
 {
-    _playingPieces = [[NSMutableArray alloc] init];
+    _playingPieces = [[NSMutableDictionary alloc] init];
+    NSArray *playingPieceNames = @[@"F", @"I", @"L", @"N", @"P", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z"];
     
-    NSArray *images = [[NSArray alloc] initWithObjects:
-                       [UIImage imageNamed:@"tileF.png"],
-                       [UIImage imageNamed:@"tileI.png"],
-                       [UIImage imageNamed:@"tileL.png"],
-                       [UIImage imageNamed:@"tileN.png"],
-                       [UIImage imageNamed:@"tileP.png"],
-                       [UIImage imageNamed:@"tileT.png"],
-                       [UIImage imageNamed:@"tileU.png"],
-                       [UIImage imageNamed:@"tileV.png"],
-                       [UIImage imageNamed:@"tileW.png"],
-                       [UIImage imageNamed:@"tileX.png"],
-                       [UIImage imageNamed:@"tileY.png"],
-                       [UIImage imageNamed:@"tileZ.png"],
-                       nil];
-    
-    for (UIImage *image in images){
-        // Make the imageView frame half the size of the image, as the images are high resolution
-        CGRect newFrame = CGRectMake(0.0, 0.0, image.size.width/2, image.size.height/2);
+    for (NSString *name in playingPieceNames)
+    {
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"tile%@", name]];
         UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
-        imageView.frame = newFrame;
-        [self.playingPieces addObject:imageView];
+        imageView.frame = CGRectMake(0.0, 0.0, image.size.width/2, image.size.height/2);
+        [self.playingPieces setObject:imageView forKey:name];
     }
 }
 
@@ -88,8 +84,10 @@
     CGFloat originY = self.boardView.frame.origin.y + self.boardView.frame.size.height + kPaddingBetweenPiecesAndBoard;
     CGPoint currentOrigin = CGPointMake(originX, originY);
     
-    for (UIImageView *imageView in self.playingPieces){
+    for (id key in self.playingPieces){
         
+        UIImageView *imageView = [self.playingPieces objectForKey:key];
+        imageView.transform = CGAffineTransformIdentity;
         // If there isn't enough space at the end of the row, start a new row
         if (rowSpaceRemaining < imageView.frame.size.width + kEdgePaddingForPlayingPieces){
             rowSpaceRemaining = self.view.bounds.size.width - kEdgePaddingForPlayingPieces;
@@ -98,15 +96,67 @@
         }
         
         imageView.frame = CGRectMake(currentOrigin.x, currentOrigin.y, imageView.frame.size.width, imageView.frame.size.height);
-        [self.view addSubview:imageView];
         rowSpaceRemaining -= imageView.frame.size.width + kPaddingBetweenPlayingPieces;
         currentOrigin.x = self.view.bounds.size.width - rowSpaceRemaining;
+        [imageView convertPoint:imageView.frame.origin toView:self.view];
+        [self.view addSubview:imageView];
+    }
+}
+
+-(void) resetPlayingPieces
+{
+    if (self.puzzleSolved){
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:1.0];
+        [self placePlayingPieces];
+        [UIView commitAnimations];
+        self.puzzleSolved = NO;
     }
 }
 
 - (IBAction)boardButtonClicked:(id)sender {
     UIButton *boardButton = sender;
-    self.currentBoardImage = self.boardImages[boardButton.tag];
-    [self.boardView setImage: self.currentBoardImage];
+    self.currentBoardNumber = boardButton.tag;
+    [self.boardView setImage: self.boardImages[boardButton.tag]];
+    
+    if(self.puzzleSolved){
+        [self resetPlayingPieces];
+        self.puzzleSolved = NO;
+    }
+}
+
+- (IBAction)solveButtonClicked:(id)sender {
+    if (self.currentBoardNumber == 0 || self.puzzleSolved)
+        return;
+    
+    NSDictionary *currentSolution = self.solutions[self.currentBoardNumber - 1];
+    
+    for (id key in self.playingPieces){
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:1.0];
+        
+        NSDictionary *currentPieceSolution = [currentSolution objectForKey:key];
+        NSNumber *originX = [currentPieceSolution objectForKey:@"x"];
+        NSNumber *originY = [currentPieceSolution objectForKey:@"y"];
+        NSNumber *rotations = [currentPieceSolution objectForKey:@"rotations"];
+        NSNumber *flips = [currentPieceSolution objectForKey:@"flips"];
+        
+        UIImageView *imageView = [self.playingPieces objectForKey:key];
+        imageView.transform = CGAffineTransformMakeRotation(M_PI_2*[rotations floatValue]);
+        if([flips integerValue]){
+            imageView.transform = CGAffineTransformScale(imageView.transform, -1.0, 1.0);
+        }
+        imageView.frame = CGRectMake(kBoardBlockDimension*[originX floatValue], kBoardBlockDimension*[originY floatValue], imageView.frame.size.width, imageView.frame.size.height);
+        [imageView convertPoint:imageView.frame.origin toView:self.boardView];
+        [self.boardView addSubview:imageView];
+        
+        [UIView commitAnimations];
+    }
+    
+    self.puzzleSolved = YES;
+}
+
+- (IBAction)resetButtonClicked:(id)sender {
+    [self resetPlayingPieces];
 }
 @end
